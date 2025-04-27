83fe401d3d7f0cb38c12066b2d16c008fe754c4c
ThunderDog22 <116789666+ThunderDog22@users.noreply.github.com>
2025-04-27 13:59:47 +1000

    Update streamlit_app.py
    added calls/requests to my.smoothiefroot.com

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import snowflake.connector
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
@@ -61,3 +62,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered, '+ name_on_order + '!', icon="✅")
+
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
